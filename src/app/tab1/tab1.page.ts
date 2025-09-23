8beab5af9315c9d86e6c6829b9aadfcd63cf28e2
MandraVEVO <paratrabajosquesenecesiten15@gmail.com>
2025-09-23 14:45:49 -0600

    implementation of tab1

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonText, IonRow, IonCol, IonCard, IonLabel, IonButton, IonIcon } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { addIcons } from 'ionicons';
+import { ellipsisVertical } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonIcon, IonButton, IonCard, IonCol, IonRow, IonText, IonGrid, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent,IonCard, IonCol, IonRow, IonText, IonGrid,IonLabel],  
 })
 export class Tab1Page {
-  constructor() {}
+  constructor( )
+   {
+    addIcons({  ellipsisVertical });
+   }
 }
